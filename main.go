77c9e8569ae78891e908d191c1af3241663cd8da
Danila Gavrilov <deltadan@yandex.ru>
2022-07-21 10:45:10 +0300

    Encoding customers to json

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
+	// define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
+
+	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Alex", "Moscow", "111121"},
+		{"Maria", "Kazan", "110055"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
 }
